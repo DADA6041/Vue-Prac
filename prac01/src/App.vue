2d0666d10ae6c1a472fd7a56e1ca87ba52640b6f
DADAKIM <d.ya96v@gmail.com>
2023-03-28 21:37:41 +0900

    데이터 바인딩

--- prac01/src/App.vue
+++ prac01/src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <h4 class="red" :style="changeColor">다다 원룸</h4> -->
+    <!-- HTML속성도 데이터 바인딩 가능! :속성="데이터이름"-->
+    <h4>{{products[0]}}</h4>
+    <p>100 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>70 만원</p>
+    <!-- <p>{{ price2 }} 만원</p> --> <!-- 실시간 자동 렌더링 하기 위해 데이터 바인딩 사용 -->
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>100 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() { // 데이터 보관함
+    return { // object 자료로 저장할 것
+      // price1: 100,
+      // price2: 60,
+      changeColor: "color : blue",
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"]
+    }
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
